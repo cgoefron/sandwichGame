552f878a2f6a7c933a82e2479f6fc27eff64d929
Claire Hu <hum085@newschool.edu>
2016-12-12 14:17:31 -0500

    Flashing icons in Intruction

--- Assets/blinkImage.cs
+++ Assets/blinkImage.cs
@@ -3,13 +3,57 @@ using System.Collections;
 
 public class blinkImage : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject image;
+
+	bool blink = false;
+	int counter = 0;
+	public float blinkStart;
+
+	bool startFlashing = false;
+
+	void Start(){
+		StartCoroutine(Flash(10f));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update()
+	{
+		/*
+		if (counter == blinkSpeed) {
+			blink = true;
+			counter = 0;
+		} else {
+			blink = false;
+		}
+
+		counter ++;
+
+		if (blink) {
+			image.SetActive (true);
+
+			counter = 0;
+		} else {
+			image.SetActive (false);
+		}
+		*/
 	}
+
+	IEnumerator Flash(float flashCount)
+	{
+		for(float i = 0f; i < flashCount; i += 0.1f)
+		{
+
+			if (!startFlashing) {
+				yield return new WaitForSeconds (blinkStart);
+				startFlashing = true;
+			}
+
+			if (startFlashing) {
+				image.SetActive (true);
+				yield return new WaitForSeconds (2f);
+				image.SetActive (false);
+				yield return new WaitForSeconds (0.3f);
+			}
+		}
+	}
+
 }
